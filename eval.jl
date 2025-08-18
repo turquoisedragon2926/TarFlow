29dbf19f493ec540ef3a5e6238b8ee8a3c8013f9
Richard Rex <turquoisedragon2926@gmail.com>
2025-08-18 16:50:10 +0000

    eval

--- eval.jl
+++ eval.jl
@@ -1,5 +1,5 @@
 # To visualize samples
-using Images, Plots
+using Images, Plots, Statistics
 using JLD2
 using Flux
 using LinearAlgebra
@@ -14,7 +14,7 @@ include("block.jl")
 include("model.jl")
 include("utils.jl")
 
-
+plots_dir = "plots_64"
 data = JLD2.load("vel_ckpts/checkpoint_10.jld2")
 model = data["model"];
 θ = data["θ_cpu"];
@@ -33,21 +33,23 @@ heatmap(x_back[end][1, 1, :, :]')
 
 norm(x_back[end][1, 1, :, :] .- x[1, 1, :, :])
 
-samples = JLD2.load("vel_ckpts_new/checkpoint_15.jld2", "samples");
-using Statistics
+data = JLD2.load("vel_ckpts_new/checkpoint_15.jld2")
+
+samples = data["samples"];
+hp = data["hp"];
 
 for sample_idx in 1:10
     anim = @animate for i in 1:size(samples[sample_idx], 1)
         heatmap(samples[sample_idx][i][1, 1, :, :]', title="Flow $(i)")
     end
-    gif(anim, "plots/samples_$(sample_idx).gif", fps=1)
+    gif(anim, "$(plots_dir)/samples_$(sample_idx).gif", fps=1)
 end
 
 # Create animation of samples
 anim = @animate for sample_idx in 1:10
     heatmap(samples[sample_idx][end][1, 1, :, :]', title="Sample $(sample_idx)")
 end
-gif(anim, "plots/all_samples.gif", fps=1)
+gif(anim, "$(plots_dir)/all_samples.gif", fps=1)
 
 losses = data["losses"]
 l2_terms = data["l2_terms"]
@@ -56,25 +58,25 @@ ld_terms = data["ld_terms"]
 plot(l2_terms, label="l2_terms", title="Losses", xlabel="Step", ylabel="Loss")
 plot!(ld_terms, label="ld_terms")
 plot!(losses, label="losses")
-savefig("plots/losses.png")
+savefig("$(plots_dir)/losses.png")
 
 # Create combined plot with samples and hyperparameters
 param_text = """
 Hyperparameters:
-epochs: 10
-steps_per_epoch: 20
-batch: 32
-channels: 256 
-patch: 64
-H: 512
-W: 256
-C: 1
-save_every: 10
-attn_layers_per_block: 8
-num_blocks: 32
-head_dim: 8
-expansion: 4
-noise_std: 0.2
+epochs: 15
+steps_per_epoch: $(hp.steps_per_epoch)
+batch: $(hp.batch)
+channels: $(hp.channels)
+patch: $(hp.patch)
+H: $(hp.H)
+W: $(hp.W)
+C: $(hp.C)
+save_every: $(hp.save_every)
+attn_layers_per_block: $(hp.attn_layers_per_block)
+num_blocks: $(hp.num_blocks)
+head_dim: $(hp.head_dim)
+expansion: $(hp.expansion)
+noise_std: $(hp.noise_std)
 """
 
 # Create 3-panel plot
@@ -91,11 +93,11 @@ anim = @animate for sample_idx in 1:10
     p1 = heatmap(samples[sample_idx][end][1, 1, :, :]', title="Sample $(sample_idx)")
     plot(p1, p2, p3, layout=l, size=(1500,400))
 end
-gif(anim, "plots/details.gif", fps=1)
+gif(anim, "$(plots_dir)/details.gif", fps=1)
 
 dataset = VelocityModelDataset("data/x_data_no_zero.jld2", subsample_size_x=8, subsample_size_y=4)
 x = get_data(dataset, 1; batch=10)
 anim = @animate for i in 1:size(x, 1)
     heatmap(x[i, 1, :, :]', title="Sample $(i)")
 end
-gif(anim, "plots/x_data.gif", fps=1)
+gif(anim, "$(plots_dir)/x_data.gif", fps=1)
